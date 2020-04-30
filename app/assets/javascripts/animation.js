e93ceb6633c31785cbf4dc7b1216b90f1b13ea27
ohnitakahiro <ohnitakahiro@gmail.com>
2020-04-30 18:55:27 +0900

    ユーザー詳細画面をタブ切り替え実装

--- app/assets/javascripts/animation.js
+++ app/assets/javascripts/animation.js
@@ -1,59 +1,5 @@
 // ボタンを押したときのアクション
-$(function(){
-  $('#NewRecordBtn').click(function(){
-    $('#NewRecord').fadeIn();
-  });
-
-  $('.Cancel').click(function(){
-    $('#NewRecord').fadeOut();
-  });
-
-  $('#EditBtn').click(function(){
-    $('#EditRecord').fadeIn();
-  });
-
-  $('.Cancel').click(function(){
-    $('#EditRecord').fadeOut();
-  });
-
-  $('#CalenderIcon').click(function(){
-    $('#Calender-show').fadeIn();
-  });
-
-  $('.Cancel').click(function(){
-    $('#Calender-show').fadeOut();
-  });
-
-  $(function () {
-    var idNo = 1;
-    $('#Menu_add_btn').click(function() {
-      $("div#Menu_TemplateForm")
-      .clone(true)
-      .removeAttr("id")
-            // 非表示解除
-            .removeClass("hide")
-            // テキストボックスのID追加
-            // .find("input[name=templateTextbox]")
-            // .attr("id", "textbox_" + idNo)
-            // .end()
-            // 情報表示
-            // .find("span.dispInfo")
-            // .text("id[" + idNo + "] TextBox_ID[" + "textbox_" + idNo + "] Button_ID:[" + "button_" + idNo + "]")
-            // .end()
-            // 追加処理
-            .appendTo("div#kokoniikitai");
-  
-        // ID番号加算
-        // idNo++;
-    });
-  });
-
-  // $(function () {
-  //   $('#Menu_sets_add_btn').click(function() {
-  //     $(".Menu_addform").clone().appendTo('#Menu_form_hide');
-  //   });
-  // });
-
+$(function(){ 
   jQuery(function($) {
     var topBtn = $('#btn_scroll_top');
     topBtn.hide();
@@ -71,6 +17,29 @@ $(function(){
         return false;
     });
 
+  });
+
+  // タブ切り替え
+  $(function() {
+    //クリックしたときのファンクションをまとめて指定
+    $('.tab li').click(function() {
+  
+      //.index()を使いクリックされたタブが何番目かを調べ、
+      //indexという変数に代入します。
+      var index = $('.tab li').index(this);
+  
+      //コンテンツを一度すべて非表示にし、
+      $('.content li').css('display','none');
+  
+      //クリックされたタブと同じ順番のコンテンツを表示します。
+      $('.content li').eq(index).css('display','block');
+  
+      //一度タブについているクラスselectを消し、
+      $('.tab li').removeClass('select');
+  
+      //クリックされたタブのみにクラスselectをつけます。
+      $(this).addClass('select')
+    });
   });
 
 });
